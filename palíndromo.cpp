e90a9bca166a3f0d4464214c8609b0817234a1ee
axelireta <axel.ireta@comunidad.unam.mx>
2025-01-10 21:09:19 -0600

    Mi primer cambio

--- palíndromo.cpp
+++ palíndromo.cpp
@@ -1,35 +1,38 @@
 //
 // Created by usuario on 09/01/2025.
-//
+
 #include <stdio.h>
 #include <string.h>
+
 //Prototipos de funciones
-int isPalindrome(char*word);
-void getInput(char*word);
+int isPalindrome(char *word);
+void getInput(char *word);
+
 
 int main(){
-  void getInput(char*word);
+  char word[22];
+  getInput(word);
+  return 0;
 }
 
-int palabra [23];
-
-int isPalindrome(char*word) {
+int isPalindrome(char *word) {
   int i;
   int longitud=strlen(word);
   for (i=0; i<longitud/2; i++) {
     if (word[i] != word[longitud-i-1]) {
-      return isPalindrome(word);
+      return 0;
     }
-  } return 0;
+  }
+  return 1;
 }
 
-void getInput(char*word) {
-  word[23];
+void getInput(char *word) {
+  word[22];
   printf("Por favor ingresa una palabra");
   scanf("%s", word);
   if (isPalindrome(word)) {
-    printf("La palabra es un palíndromo");
+    printf("La palabra es un palindromo");
   } else {
     printf("La palabra no es un palindromo");
-    }
+  }
 }
